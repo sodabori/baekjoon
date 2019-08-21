8703a3d73f3cfb9eac473fb727d2b0c09efacb4c
Ji Woong Shin <jwshin0610@postech.ac.kr>
2019-08-21 15:58:11 +0900

    10844(6)

--- 10844.cpp
+++ 10844.cpp
@@ -12,14 +12,17 @@ long number_count(int cur, int n) {
 	if (n == 0)
 		return 1;
 
-	if (counts[cur][n] != 0)
+	if (counts[cur][n] != 0) {
+		printf("Use Cache!!!\n");
 		return counts[cur][n];
+	}
 
 	if (cur == 9)
 		cd1 = 0;
 	else {
 		cd1 = number_count(cur + 1, n - 1) % DIVIDER;
 		counts[cur + 1][n - 1] = cd1;
+		printf("(%d, %d) -> (%d, %d)\n", cur, n, cur + 1, n - 1);
 	}
 
 	if (cur == 0)
@@ -27,6 +30,7 @@ long number_count(int cur, int n) {
 	else {
 		cd2 = number_count(cur - 1, n - 1) % DIVIDER;
 		counts[cur - 1][n - 1] = cd2;
+		printf("(%d, %d) -> (%d, %d)\n", cur, n, cur - 1, n - 1);
 	}
 
 	return (cd1 + cd2) % DIVIDER;
@@ -37,8 +41,10 @@ int main() {
 	long sum = 0;
 
 	scanf("%d", &n);
-	for (int i = 1; i < 10; i++)
+	for (int i = 1; i < 10; i++) {
 		sum += number_count(i, n - 1);
+		sum = sum % DIVIDER;
+	}
 	printf("%ld\n", sum);
 
 	return 0;
