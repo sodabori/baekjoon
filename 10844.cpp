5dbc1573a6b575801dfb791ea2a5513bc1010b54
Ji Woong Shin <jwshin0610@postech.ac.kr>
2019-08-21 15:12:16 +0900

    10844(5)

--- 10844.cpp
+++ 10844.cpp
@@ -2,10 +2,10 @@
 #define MAX_N 100
 #define DIVIDER 1000000000
 
-long long counts[10][MAX_N + 1];
+long counts[10][MAX_N + 1];
 
-long long number_count(int cur, int n) {
-	long long cd1, cd2;
+long number_count(int cur, int n) {
+	long cd1, cd2;
 	if (cur < 0 || cur > 9)
 		return 0;
 
@@ -15,25 +15,31 @@ long long number_count(int cur, int n) {
 	if (counts[cur][n] != 0)
 		return counts[cur][n];
 
-	cd1 = number_count(cur + 1, n - 1) % DIVIDER;
-	cd2 = number_count(cur - 1, n - 1) % DIVIDER;
-
-	if (cur < 9)
+	if (cur == 9)
+		cd1 = 0;
+	else {
+		cd1 = number_count(cur + 1, n - 1) % DIVIDER;
 		counts[cur + 1][n - 1] = cd1;
-	if (cur > 0)
+	}
+
+	if (cur == 0)
+		cd2 = 0;
+	else {
+		cd2 = number_count(cur - 1, n - 1) % DIVIDER;
 		counts[cur - 1][n - 1] = cd2;
+	}
 
 	return (cd1 + cd2) % DIVIDER;
 }
 
 int main() {
 	int n;
-	long long sum = 0;
+	long sum = 0;
 
 	scanf("%d", &n);
 	for (int i = 1; i < 10; i++)
 		sum += number_count(i, n - 1);
-	printf("%lld\n", sum);
+	printf("%ld\n", sum);
 
 	return 0;
 }
